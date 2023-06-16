6758231d10ef6b0ad2b3bf1b24a3175f2a18f593
denis177788 <126872732+denis177788@users.noreply.github.com>
2023-06-16 16:21:29 +0300

    Add files via upload

---
+++ project/fpages/admin.py
@@ -0,0 +1,36 @@
+# данный файл я создал из задания Skillfactory
+#
+# цель - чтобы появилась панель:
+#
+# Advanced options (Hide)
+#
+# Enable comments
+#
+# Registration required
+# If this is checked, only logged-in users will be able to view the page.
+
+
+from django.contrib import admin
+from django.contrib.flatpages.admin import FlatPageAdmin
+from django.contrib.flatpages.models import FlatPage
+from django.utils.translation import gettext_lazy as _
+
+
+# Define a new FlatPageAdmin
+class FlatPageAdmin(FlatPageAdmin):
+    fieldsets = (
+        (None, {'fields': ('url', 'title', 'content', 'sites')}),
+        (_('Advanced options'), {
+            'classes': ('collapse',),
+            'fields': (
+                'enable_comments',
+                'registration_required',
+                'template_name',
+            ),
+        }),
+    )
+
+
+# Re-register FlatPageAdmin
+admin.site.unregister(FlatPage)
+admin.site.register(FlatPage, FlatPageAdmin)
